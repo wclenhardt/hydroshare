fdcf8b35c510f2c1409a6e166bbab320dcce950f
hyi <hongyi@renci.org>
2015-09-11 15:36:23 -0400

    added mapping from old resource id to new resource id via resource short_id when loading comments and ratings into new database

---
+++ migration-scripts/loadRatingsFromJSON.py
@@ -0,0 +1,31 @@
+# usage: python loadRatingsFromJSON.py rating.JSON resources.JSON new_resources.JSON
+# where resources.JSON is used to find mapping from object_pk to short_id and
+# new_resources.JSON is used to find mapping from short_id to object_pk for new resources
+import os
+import sys
+import json
+
+os.environ.setdefault("PYTHONPATH", '/home/docker/hydroshare')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'hydroshare.settings'
+
+import django
+from django.core import serializers
+
+django.setup()
+
+id_to_short_id = {}
+with open(sys.argv[2]) as old_res_file:
+    for old_res in serializers.deserialize("json", old_res_file):
+        id_to_short_id[old_res.object.pk] = old_res.object.short_id
+
+short_id_to_id = {}
+with open(sys.argv[3]) as new_res_file:
+    for new_res in serializers.deserialize("json", new_res_file):
+        short_id_to_id[new_res.object.short_id] = new_res.object.pk
+
+with open(sys.argv[1]) as json_file:
+    for rating in serializers.deserialize("json", json_file):
+        short_id = id_to_short_id[rating.object.object_pk]
+        rating.object.object_pk = short_id_to_id[short_id]
+        rating.save()
+
